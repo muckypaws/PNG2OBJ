b4dc3c20af0cd00dabc685f1a2402ed21d27bc0f
muckypaws <jason@muckypaws.com>
2022-06-05 19:48:44 +0100

    V1.01 Added Jointing Option for 3D Printing Hashed Pattern Pixels

--- PNG2Obj.py
+++ PNG2Obj.py
@@ -10,6 +10,9 @@
 # There's much that can be done to optimise the code
 #
 # V1.00 - 3rd June 2022 - The Platty Joobs Edition ;)
+# V1.01 - 5th June 2022 - The Platty Pudding Edition ;)
+#                         Add Jointer Cubes for diagonal pixels without support
+#                         Only needed if you're using a printer.
 #
 # Usage :-
 #   PNG2OBJ.py Filename
@@ -36,8 +39,8 @@ PATTERNS = "./"
 
 # Set Pixel Width and Height if Sprites are fixed width within the sheet, otherwise 
 # use a high number.
-Pixel_W = 16
-Pixel_H = 16
+Pixel_W = 1024
+Pixel_H = 1024
 
 # X is positive, Y is negative unless you want to flip the image.
 CUBE_X = 10
@@ -46,49 +49,95 @@ CUBE_Y = -10
 # Define the Alpha Value as Cut Off For the Pixel
 ALPHACUTOFF = 200
 
+# Define TRUE if Printing, FALSE if anything else as not required
+JOINTS_REQUIRED = True
+
 # Used to define the Current Face Counter
 # Needed to ensure Vertices are correctly defined.
 Current_Face = 0
 
 # Basic Definition of cube with 24 Vertices (4 Sets of Co-Ords per face)
-primitive_vertices = ([ 10.000000,  0.000000, 10.000000],
-                [ 10.000000, 10.000000, 10.000000],
-                [  0.000000, 10.000000, 10.000000],
-                [  0.000000,  0.000000, 10.000000],
-                [ 10.000000, 10.000000,  0.000000],
-                [ 10.000000,  0.000000,  0.000000],
-                [  0.000000,  0.000000,  0.000000],
-                [  0.000000, 10.000000,  0.000000],
-                [  0.000000,  0.000000, 10.000000],
-                [  0.000000,  0.000000,  0.000000],
-                [ 10.000000,  0.000000,  0.000000],
-                [ 10.000000,  0.000000, 10.000000],
-                [  0.000000, 10.000000, 10.000000],
-                [  0.000000, 10.000000,  0.000000],
-                [  0.000000,  0.000000,  0.000000],
-                [  0.000000,  0.000000, 10.000000],
-                [ 10.000000, 10.000000, 10.000000],
-                [ 10.000000, 10.000000,  0.000000],
-                [  0.000000, 10.000000,  0.000000],
-                [  0.000000, 10.000000, 10.000000],
-                [ 10.000000,  0.000000, 10.000000],
-                [ 10.000000,  0.000000,  0.000000],
-                [ 10.000000, 10.000000,  0.000000],
-                [ 10.000000, 10.000000, 10.000000])
+cube_vertices = (   [  0.000000, 10.000000, 10.000000],
+                    [ 10.000000, 10.000000, 10.000000],
+                    [ 10.000000, 10.000000,  0.000000],
+                    [  0.000000, 10.000000,  0.000000],
+                    [ 10.000000, 10.000000, 10.000000],
+                    [ 10.000000,  0.000000, 10.000000],
+                    [ 10.000000,  0.000000,  0.000000],
+                    [ 10.000000, 10.000000,  0.000000],
+                    [ 10.000000,  0.000000, 10.000000],
+                    [  0.000000,  0.000000, 10.000000],
+                    [  0.000000,  0.000000,  0.000000],
+                    [ 10.000000,  0.000000,  0.000000],
+                    [  0.000000,  0.000000, 10.000000],
+                    [  0.000000, 10.000000, 10.000000],
+                    [  0.000000, 10.000000,  0.000000],
+                    [  0.000000,  0.000000,  0.000000],
+                    [ 10.000000, 10.000000, 10.000000],
+                    [  0.000000, 10.000000, 10.000000],
+                    [  0.000000,  0.000000, 10.000000],
+                    [ 10.000000,  0.000000, 10.000000],
+                    [  0.000000,  0.000000,  0.000000],
+                    [  0.000000, 10.000000,  0.000000],
+                    [ 10.000000, 10.000000,  0.000000],
+                    [ 10.000000,  0.000000,  0.000000])
 
 #Basic Definition of a Primitives connecting faces 
-primitive_faces = ([  2,  3,  1],
-            [  1,  3,  4],
-            [  6,  7,  5],
-            [  5,  7,  8],
-            [  9, 10, 12],
-            [ 12, 10, 11],
-            [ 13, 14, 16],
-            [ 16, 14, 15],
-            [ 17, 18, 20],
-            [ 20, 18, 19],
-            [ 21, 22, 24],
-            [ 24, 22, 23])
+cube_faces = (  [ 1,  2,  4],
+                [ 4,  2,  3],
+                [ 5,  6,  8],
+                [ 8,  6,  7],
+                [ 9, 10, 12],
+                [12, 10, 11],
+                [13, 14, 16],
+                [16, 14, 15],
+                [17, 18, 20],
+                [20, 18, 19],
+                [22, 23, 21],
+                [21, 23, 24])
+
+
+# Basic Definition for a Jointer Cube Where Pixels are connected by Corners Only
+# This is needed if there's no overlap on your printer and want to ensure a 
+# a connection that should lift off the printer.
+
+joint_verticies = ( [  7.171573,  0.000000, 7.000000],
+                    [ 10.000000,  2.828427, 7.000000],
+                    [ 10.000000,  2.828427, 3.000000],
+                    [  7.171573,  0.000000, 3.000000],
+                    [ 10.000000,  2.828427, 7.000000],
+                    [ 12.828427, -0.000000, 7.000000],
+                    [ 12.828427, -0.000000, 3.000000],
+                    [ 10.000000,  2.828427, 3.000000],
+                    [ 12.828427, -0.000000, 7.000000],
+                    [ 10.000000, -2.828427, 7.000000],
+                    [ 10.000000, -2.828427, 3.000000],
+                    [ 12.828427, -0.000000, 3.000000],
+                    [ 10.000000, -2.828427, 7.000000],
+                    [  7.171573,  0.000000, 7.000000],
+                    [  7.171573,  0.000000, 3.000000],
+                    [ 10.000000, -2.828427, 3.000000],
+                    [ 10.000000,  2.828427, 7.000000],
+                    [  7.171573,  0.000000, 7.000000],
+                    [ 10.000000, -2.828427, 7.000000],
+                    [ 12.828427, -0.000000, 7.000000],
+                    [ 10.000000, -2.828427, 3.000000],
+                    [  7.171573,  0.000000, 3.000000],
+                    [ 10.000000,  2.828427, 3.000000],
+                    [ 12.828427, -0.000000, 3.000000])
+
+joint_faces = ( [  1,  2,  4],
+                [  4,  2,  3],
+                [  5,  6,  8],
+                [  8,  6,  7],
+                [  9, 10, 12],
+                [ 12, 10, 11],
+                [ 13, 14, 16],
+                [ 16, 14, 15],
+                [ 17, 18, 20],
+                [ 20, 18, 19],
+                [ 21, 22, 24],
+                [ 24, 22, 23])
 
 # Update Verticies depending on position (0 or non 0)
 def update_vert(val,r1,r2):
@@ -100,7 +149,7 @@ def update_vert(val,r1,r2):
 # Create a Primitive, in this example a Cube, but could be swapped for 
 #   Any primitive type
 #
-def create_primitive(primitive_x, primitive_y, width):
+def create_primitive(primitive_x, primitive_y, width, primitive_vert, primitive_face, jointFlag):
     global Current_Face
 
     strVertices = f"o Pixel_{primitive_x}_{primitive_y}\n"
@@ -108,8 +157,8 @@ def create_primitive(primitive_x, primitive_y, width):
 
     # Get the number of entries for Cube Vertices
     # For future where primitive may change
-    vert_len = len(primitive_vertices)
-    face_len = len(primitive_faces)
+    vert_len = len(primitive_vert)
+    face_len = len(primitive_face)
 
     rx = primitive_x * CUBE_X
     ry = primitive_y * CUBE_Y
@@ -118,22 +167,30 @@ def create_primitive(primitive_x, primitive_y, width):
 
     myFormatter = "{0:.6f}"
 
+    #
+    # Generate the Basic Primitive for the Model
+    #
     for index in range(vert_len):
-        v1 = primitive_vertices[index][0]
-        v2 = primitive_vertices[index][1]
-        v3 = primitive_vertices[index][2]
+        v1 = primitive_vert[index][0]
+        v2 = primitive_vert[index][1]
+        v3 = primitive_vert[index][2]
 
-        v1 = update_vert(v1, rx, rx2)
-        v2 = update_vert(v2, ry, ry2)
+        # Yeah, quick and dirty, I need to think this through
+        if not jointFlag:
+            v1 = update_vert(v1, rx, rx2)
+            v2 = update_vert(v2, ry, ry2)
+        else:
+            v1 = v1 + rx
+            v2 = v2 + ry
 
         strVertices = strVertices + "v " + str(myFormatter.format(v1)) + " " + str(myFormatter.format(v2)) + " " + str(myFormatter.format(v3)) + "\n"
 
     strVertices = strVertices + "# "+str(vert_len)+f" Vertices\ng Extrude_{primitive_x}_{primitive_y}\n"
 
     for index in range(face_len):
-        f1 = primitive_faces[index][0] + Current_Face
-        f2 = primitive_faces[index][1] + Current_Face
-        f3 = primitive_faces[index][2] + Current_Face
+        f1 = primitive_face[index][0] + Current_Face
+        f2 = primitive_face[index][1] + Current_Face
+        f3 = primitive_face[index][2] + Current_Face
 
         strFaces = strFaces + "f " + f"{f1} {f2} {f3}\n"
 
@@ -143,7 +200,56 @@ def create_primitive(primitive_x, primitive_y, width):
     Current_Face = Current_Face + (face_len * 2)
 
     return Primitive_String
-        
+
+#
+# Convert Colour To Pixel - Can extend this to exclude colour ranges in future updates.
+#
+def getPixelFromRow(x, row, channels, rowWidth):
+    # Calculate the offset into the ROW for pixel information
+    offset_x = (x * channels) % (rowWidth * channels)
+    pixel = 0
+
+    # If Indexed PNG, we're only interested in the index value
+    if channels == 1:
+        pixel = row[x]
+
+    else:
+        # Get the RGB Values from the offset
+        r, g, b = row[offset_x:offset_x + 3]
+
+        # Crude but effective as we're only interested in whether colour data
+        # is prsent or not to determine whether to add a primitive or not
+        pixel = r|g|b
+
+    if channels == 4:
+        #Check Alpha
+        a = row[offset_x+3]
+        # If below the cutoff we set the pixel to 0 (No Colour Data)
+        if a < ALPHACUTOFF:
+            pixel = 0
+
+    return pixel
+
+#
+# Process a simple set of rules to determine if a Jointer Block is required.
+# That is missing diagonal pixes.  It's crude and needs refinement.
+#
+def CheckJointRequired(x ,row, nextRow, channels, pattern_w):
+    isJointRequired = False
+
+    # Not Checking last pixel as this is covered 
+    if x >= (pattern_w-1): 
+        return isJointRequired
+
+    a = getPixelFromRow(x,   row,     channels, pattern_w)
+    b = getPixelFromRow(x+1, row,     channels, pattern_w)
+    c = getPixelFromRow(x,   nextRow, channels, pattern_w)
+    d = getPixelFromRow(x+1, nextRow, channels, pattern_w)
+
+    if (a > 0 and d > 0 and b == 0 and c == 0) or (b > 0 and c > 0 and a == 0 and d == 0):
+        isJointRequired = True
+
+    return isJointRequired
 #
 # And this is where all that magic happens.
 #   A real Python Programmer can probably optimise this using some Python Magic.
@@ -178,6 +284,9 @@ def main():
     obj_file = os.path.join(PATTERNS, "{}.obj".format(sys.argv[1]))
     txt_file = os.path.join(PATTERNS, "{}.txt".format(sys.argv[1]))
 
+    # If we're adding Jointer Blocks this will be required.
+    LastRow = False
+
     # open files for Writing, Note we're not checking their presence as we're overwriting/creating
     #   from scratch each time.
     try:
@@ -197,7 +306,18 @@ def main():
                 # Work our way through each row of the PNG File.
                 for y in range(pattern_h):
                     row = pattern[int(y % pattern_h)]
+                    
+                    # Get Next Row for Jointer Block Processing.
+                    # We're cheating by MOD by the PNG Height
+                    # But will set the Last row flag so we don't 
+                    # Process the rules for this.
+                    nextRow = pattern[int((y+1) % pattern_h)]
+
                     fp_txt.write('\n')
+
+                    # Check If We're processing the last row of Pixels
+                    if y == (pattern_h-1):
+                        LastRow = True
 
                     # If we're splitting models based on pixel width and height add and extra line
                     #   And ensure we start the next primitive further down to enforce a gap in the model
@@ -218,29 +338,13 @@ def main():
                             start_x=start_x+1
 
                             if pixel_found:
-                                fp_obj.write(  create_primitive(primitive_x, start_y, primitive_width) )
+                                fp_obj.write(  create_primitive(primitive_x, start_y, primitive_width, cube_vertices, cube_faces, newJoint) )
                                 pixel_found = False
                                 primitive_width = 0
                                 Total_Primitives += 1
-                        
-                        # Calculate the offset into the ROW for pixel information
-                        offset_x = (x * channels) % (pattern_w * channels)
 
-                        # Get the RGB Values from the offset
-                        r, g, b = row[offset_x:offset_x + 3]
-
-                        # Crude but effective as we're only interested in whether colour data
-                        # is prsent or not to determine whether to add a primitive or not
-                        pixel = r|g|b
-
-                        # If there's an ALPHA Channel present, check to see if it meets the
-                        # minimum threshold to continue
-                        if channels == 4:
-                            #Check Alpha
-                            a = row[offset_x+3]
-                            # If below the cutoff we set the pixel to 0 (No Colour Data)
-                            if a < ALPHACUTOFF:
-                                pixel = 0
+                        # Get Pixel from Row
+                        pixel = getPixelFromRow(x, row, channels, pattern_w)
 
                         # If Pixel present then add to TXT File and create primitive.
                         if pixel > 0:
@@ -254,18 +358,29 @@ def main():
                             #fp_obj.write(  create_primitive(start_x, start_y) )
                         else:
                             if pixel_found:
-                                fp_obj.write(  create_primitive(primitive_x, start_y, primitive_width) )
+                                fp_obj.write(  create_primitive(primitive_x, start_y, primitive_width, cube_vertices, cube_faces, newJoint) )
                                 pixel_found = False
                                 primitive_width = 0
                                 Total_Primitives += 1
                             fp_txt.write(" ")
+
+                        # Check if we're to add Jointer Blocks
+                        if JOINTS_REQUIRED and not LastRow:
+                            # Check if Blocks meet the Jointer rule
+                            newJoint = CheckJointRequired(x,row, nextRow, channels, pattern_w)
+
+                            if newJoint:
+                                fp_obj.write(  create_primitive(start_x, start_y + 1, 1, joint_verticies, joint_faces, newJoint) )
+                                newJoint = False
+
+
 
                         # Update X Position (Taking into account an offset if we're adding space between sprites)
                         start_x = start_x + 1
 
                     # Update to the next Y Postion and check if we have an unwritted primitive to complete
                     if pixel_found:
-                        fp_obj.write( create_primitive(primitive_x, start_y, primitive_width) )
+                        fp_obj.write( create_primitive(primitive_x, start_y, primitive_width, cube_vertices, cube_faces, newJoint) )
                         pixel_found = False
                         primitive_width = 0
                         primitive_x = 0
